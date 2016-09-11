b2555626df13af4047361020b683086135630178
naito <ws.hiroshi.naito@gmail.com>
2016-09-11 07:11:23 +0000

    edit other's

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911044615) do
+ActiveRecord::Schema.define(version: 20160911062553) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+# Could not dump table "users" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
