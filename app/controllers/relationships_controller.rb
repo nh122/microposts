eeb1e3b15bb5fe0fb2aba7e55952b0f24c046eb8
naito <ws.hiroshi.naito@gmail.com>
2016-09-16 10:07:34 +0000

    relation mawari chosei chu

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @user = User.find(params[:followed_id])
-    current_user.follow(@user)
+    @follow_user = User.find(params[:followed_id])
+    current_user.follow(@follow_user)
   end
 
   def destroy
-    @user = current_user.following_relationships.find(params[:id]).followed
-    current_user.unfollow(@user)
+    @follow_user = current_user.following_relationships.find(params[:id]).followed
+    current_user.unfollow(@follow_user)
   end
 end
